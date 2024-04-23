ec69849643f2b360a8bb2b89fad307422d22fc80
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-04-23 20:42:04 +0530

    "Add CardDisplayGrocery component to Home page; update CSS for Silider and Home components"

--- src/Components/Cards/CardDisplayGrocery/CardDisplayGrocery.jsx
+++ src/Components/Cards/CardDisplayGrocery/CardDisplayGrocery.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import StyleCardDisplayGrocery from './CardDisplayGrocery.module.css'
 
-const CardDisplayGrocery = () => {
+const CardDisplayGrocery = ({image, name,border, backgroundColor}) => {
   return (
     <>
-    <div>CardDisplayGrocery</div>
+    <div className={StyleCardDisplayGrocery.card}
+    style={{border: `${border}`,  background: `${backgroundColor}`}}
+    >
+        <span>
+            <img src={image} alt="grocery" style={{width: 'auto', height: '111px'}} />
+        </span>
+        <br/>
+        <span className={StyleCardDisplayGrocery.text}>{name}</span>
+    </div>
     </>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,14 +4,16 @@ import BottomNav from "../../Components/BottomNav/BottomNav";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import Silider from "../../Components/Silider/Silider";
 import MidBar from "../../Components/MidBar/MidBar";
+import CardDisplayGrocery from "../../Components/Cards/CardDisplayGrocery/CardDisplayGrocery";
 
 const Home = () => {
   return (
     <>
       <div className={StylesHome.container}>
-        <div>
+        <div className={StylesHome.SearchBar}>
           <SearchBar />
         </div>
+        <div className={StylesHome.top}></div>
         <div>
           <Silider />
         </div>
@@ -19,8 +21,43 @@ const Home = () => {
           <MidBar />
         </div>
         <div className={StylesHome.shop}>
-          <div className={StylesHome.textShop}><br/>SHOP BY CATEGORY</div>
+          <div className={StylesHome.textShop}>
+            <br />
+            SHOP BY CATEGORY
+          </div>
           <div className={StylesHome.textGrocery}>Grocery</div>
+          <div style={{display: "flex", justifyContent: "space-evenly", flexWrap: "wrap" , gap:"21px"}}>
+            <CardDisplayGrocery
+              border={"1px solid rgba(83, 177, 117, 0.7)"}
+              backgroundColor={"rgba(83, 177, 117, 0.1)"}
+              name={"Frash Fruits & Vegetable"}
+              image={"https://www.transparentpng.com/thumb/vegetables/all-fruits-and-vegetables-in-basket-background-transparent-veD4qx.png"}
+            />
+            <CardDisplayGrocery
+              border={"1px solid rgba(248, 164, 76, 0.7)"}
+              backgroundColor={"rgba(248, 164, 76, 0.1)"}
+              name={"Cooking Oil & Ghee"}
+              image={"https://purepng.com/public/uploads/large/purepng.com-sunflower-oilsunflower-oilcooking-oilfrying-oilnon-volatile-oil-1411529832890igm2q.png"}
+            />
+            <CardDisplayGrocery
+              border={"1px solid #d3b0e0"}
+              backgroundColor={"rgba(211, 176, 224, 0.25)"}
+              name={"Bakery & Snacks"}
+              image={"https://png.pngtree.com/png-vector/20231213/ourmid/pngtree-gourmet-delicious-bakery-food-snacks-satiating-croissant-png-image_11322979.png"}
+            />
+            <CardDisplayGrocery
+              border={"1px solid #fde598"}
+              backgroundColor={"rgba(253, 229, 152, 0.25)"}
+              name={"Dairy & Eggs"}
+              image={"https://png.pngtree.com/png-clipart/20230504/original/pngtree-3d-food-bread-milk-eggs-three-dimensional-effect-png-image_9136670.png"}
+            />
+            <CardDisplayGrocery
+              border={"1px solid rgba(83, 177, 117, 0.7)"}
+              backgroundColor={"rgba(83, 177, 117, 0.1)"}
+              name={"Beverages"}
+              image={"https://www.freepnglogos.com/uploads/drinks-png/drinks-transparent-mojito-cocktail-png-clipart-best-web-clipart-6.png"}
+            />
+          </div>
         </div>
         <div className={StylesHome.bottom}></div>
         <div>
